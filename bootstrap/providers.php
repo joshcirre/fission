4550afef46c50c6ea0ceaed6a1b8e10d38f514db
Josh Cirre <joshcirre@gmail.com>
2025-02-12 12:32:51 -0700

    remove/update solo

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
-    App\Providers\SoloServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
